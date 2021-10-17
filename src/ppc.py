fixed sabre heuristic to lookahead

src/ppc.py
@@ -16,6 +16,7 @@ class PriorityPathCollection:
 		self.conflict_matrix = [[0 for _ in range(num_vertices)] for _ in range(num_sources)]
 		self.path_collection_list = path_collection_list
 		self.pqueues = [PathPriorityQueue.buildheap(path_collection_list[i], self.conflict_matrix, i) for i in range(num_sources)]
+		self.reduceable = [i for i in range(num_sources)]
 	
 	def find_and_join(self, verifier, target_list, current_layout, post_primary_layer_view, runs=1000):
 		min_collection_list = []	
@@ -52,13 +53,13 @@ class PriorityPathCollection:
 			if _soln_hash_f(collection) in soln_hash:
 				continue
 			soln_hash.add(_soln_hash_f(collection))
-			if is_valid and min_score > 0 and score > min_score:
-				# The cost of searching for a golden goose is too high. Just exit.
-				break
+#			if is_valid and min_score > 0 and score > min_score:
+#				# The cost of searching for a golden goose is too high. Just exit.
+#				break
 			if min_score < 0 or score < min_score:
 				min_score = score
 				min_collection_list.append(collection)
-			elif score == min_score:
+			elif min_score <= score <= 1.5*min_score:
 				min_collection_list.append(collection)
 		if len(min_collection_list) == 0:  
 			# Reset the PPC to the first tree.
@@ -72,7 +73,7 @@ class PriorityPathCollection:
 			suggestions = []
 			while dfs_stack:
 				node = dfs_stack.pop()
-				if not node.valid:
+				if not node.valid or node.conflict == 1:
 					dfs_stack.append(node.left_child)
 					dfs_stack.append(node.right_child)
 				else:
@@ -86,30 +87,28 @@ class PriorityPathCollection:
 		self.pqueues = [PathPriorityQueue.buildheap(self.path_collection_list[i], self.conflict_matrix, i) for i in range(self.num_sources)]
 	
 	def _random_select_and_update(self, path_join_tree, verifier, target_list, current_layout, post_primary_layer_view):
-		modified_heaps = []
-		new_paths = []
-		for (i, pq) in enumerate(self.pqueues):
-			# Randomly adjust the scores of used paths in the segment tree.
-			if np.random.random() < 0.5:
-				path, s = pq.peek()
-				pq.change_score(path, s * 10, self.conflict_matrix, i) 
-				modified_heaps.append(i)
-				new_paths.append(pq.peek()[0])
-		if len(modified_heaps) > 0:
-			path_join_tree.modify_leaves(modified_heaps, new_paths, verifier, target_list, current_layout, post_primary_layer_view)
+		rand_index = np.random.randint(0, high=len(self.reduceable))
+		i = self.reduceable[rand_index]
+		pq = self.pqueues[i]
+		path, s = pq.peek()
+		pq.change_score(path, s*10, self.conflict_matrix, i)
+		new_path = pq.peek()[0]
+
+		path_join_tree.modify_leaves([i], [new_path], verifier, target_list, current_layout, post_primary_layer_view) 
+		del self.reduceable[rand_index]
+		if len(self.reduceable) == 0:
+			self.reduceable = [i for i in range(self.num_sources)]
 
 	def _dfs_search_invalid_and_update(self, path_join_tree, verifier, target_list, current_layout, post_primary_layer_view):
 		dfs_stack = [path_join_tree.root]
 
 		modified_heaps = []
 		new_paths = []
-#		print('======')
 		while dfs_stack:
 			node = dfs_stack.pop()
 			left, right = node.left_child, node.right_child
 			# Only go down invalid children.
 			# If neither are invalid, then update a random entry in the target_sub_list.
-#			print(node.target_index_list, node.valid, left.target_index_list, left.valid, right.target_index_list, right.valid)
 			if left.valid == 1 and right.valid == 1:
 				update_index = node.target_index_list[np.random.randint(0, high=len(node.target_index_list))]
 				pq = self.pqueues[update_index]

src/qcirc.py
@@ -10,12 +10,14 @@ from qiskit.compiler import transpile
 from qiskit.visualization import plot_histogram
 from qiskit import Aer
 
-from mpathswap import MultipathSwap, MPSWAP_ERRNO
+from mpathswap import MultipathSwap
 from layerview import LayerViewPass
 
 from timeit import default_timer as timer
 
 from sys import argv
+
+import numpy as np
 
 G_QISKIT_OPT_NONE = 0
 G_QISKIT_OPT_LIGHT = 1
@@ -26,6 +28,9 @@ G_QISKIT_OPT_LVL = G_QISKIT_OPT_HEAVY
 G_QISKIT_GATE_SET = ['u1', 'u2', 'u3', 'cx']
 
 BACKEND = Aer.get_backend('qasm_simulator')
+
+ibm_toronto = np.array([[0, 1], [1, 0], [1, 2], [1, 4], [2, 1], [2, 3], [3, 2], [3, 5], [4, 1], [4, 7], [5, 3], [5, 8], [6, 7], [7, 4], [7, 6], [7, 10], [8, 5], [8, 9], [8, 11], [9, 8], [10, 7], [10, 12], [11, 8], [11, 14], [12, 10], [12, 13], [12, 15], [13, 12], [13, 14], [14, 11], [14, 13], [14, 16], [15, 12], [15, 18], [16, 14], [16, 19], [17, 18], [18, 15], [18, 17], [18, 21], [19, 16], [19, 20], [19, 22], [20, 19], [21, 18], [21, 23], [22, 19], [22, 25], [23, 21], [23, 24], [24, 23], [24, 25], [25, 22], [25, 24], [25, 26], [26, 25]])
+
 
 def draw(circ):
 	print(circ.draw(output='text'))
@@ -44,7 +49,7 @@ def _build_pass_manager(router, coupling_map, mapping='sabre'):
 	if router is None:
 		return PassManager([basis_pass, gate1q_pass])
 	else:
-		return PassManager([basis_pass, layout_pass, apply_layout_pass, layer_view, router, gate1q_pass]) 
+		return PassManager([basis_pass, layout_pass, apply_layout_pass, layer_view, router]) 
 
 def _pad_circuit_to_fit(circ, coupling_map):
 	while circ.num_qubits < coupling_map.size():
@@ -92,6 +97,7 @@ def _bench_and_cmp(ref_circ, coupling_map, pm0, pm1, pm2, runs=100, show=False):
 #		sabre_counts = BACKEND.run(circ1, shots=1024).result().get_counts(circ1)
 #		mpath_counts = BACKEND.run(circ2, shots=1024).result().get_counts(circ2)
 		draw(ref_circ)
+		draw(circ0)
 		draw(circ1)
 		draw(circ2)
 #		print('DIFF: [SABRE] %.3f, [MPATH] %.3f' % (_diff(base_counts, sabre_counts), _diff(base_counts, mpath_counts)))
@@ -102,19 +108,22 @@ if __name__ == '__main__':
 	n, m = int(argv[1]), int(argv[2])
 	s = max(n+m, 4)
 	circ = QuantumCircuit.from_qasm_file('benchmarks/qasmbench/%s/%s/%s.qasm' % (argv[3], argv[4], argv[4])) 
-#	circ = QuantumCircuit.from_qasm_file('benchmarks/qaoa10_depth2.qasm')
-	coupling_map = CouplingMap.from_grid(n, m)
+#	circ = QuantumCircuit.from_qasm_file('benchmarks/BV-10.qasm')
+#	circ = QuantumCircuit.from_qasm_file('benchmarks/cnot_test_n5.qasm')
+
+#	coupling_map = CouplingMap.from_grid(n, m)
+	coupling_map = CouplingMap(ibm_toronto)
 	#coupling_map = CouplingMap.from_line(n)
 	#coupling_map = CouplingMap.from_ring(n)
 
 	_pad_circuit_to_fit(circ, coupling_map)
 
-	sabre_routing_pass = SabreSwap(coupling_map)
+	sabre_routing_pass = SabreSwap(coupling_map, heuristic='lookahead')
 	mpath_routing_pass = MultipathSwap(coupling_map, max_swaps=s, max_lookahead=8, solution_cap=8)
 
 	pm0 = _build_pass_manager(None, coupling_map) 
-	pm1 = _build_pass_manager(sabre_routing_pass, coupling_map, mapping='sabre')
-	pm2 = _build_pass_manager(mpath_routing_pass, coupling_map)
+	pm1 = _build_pass_manager(sabre_routing_pass, coupling_map, mapping='trivial')
+	pm2 = _build_pass_manager(mpath_routing_pass, coupling_map, mapping='trivial')
 
 	print(_bench_and_cmp(circ, coupling_map, pm0, pm1, pm2, runs=int(argv[5]), show=True))
 